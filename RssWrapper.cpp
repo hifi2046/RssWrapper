cbb9318c810a7cf07cbf5d5993e04b3be1606582
Huang Yinfei <huangyinfei@huawei.com>
2020-04-25 09:24:28 +0800

    new file

---
+++ RssWrapper.cpp
@@ -0,0 +1,93 @@
+//#include "ad/rss/core/RssResponseResolving.hpp"
+//#include "ad/rss/core/RssResponseTransformation.hpp"
+//#include "ad/rss/core/RssSituationChecking.hpp"
+//#include "ad/rss/core/RssSituationExtraction.hpp"
+//#include "ad/rss/state/RssStateOperation.hpp"
+#include "ad/rss/core/RssCheck.hpp"
+
+void calculateOccupiedRegions(::ad::rss::world::OccupiedRegionVector &bounds, float x, float y) {
+    ::ad::rss::world::OccupiedRegion r;
+    r.segmentId = 43;
+    r.lonRange.minimum=0;
+    r.lonRange.maximum=1;
+    r.latRange.minimum=0;
+    r.latRange.maximum=1;
+    bounds.push_back(r);
+}
+
+void calculateLatLonVelocities(::ad::rss::world::Velocity &v) {
+    v.speedLonMin = ::ad::physics::Speed(20);
+    v.speedLonMax = ::ad::physics::Speed(20);
+    v.speedLatMin = ::ad::physics::Speed(0);
+    v.speedLatMax = ::ad::physics::Speed(0);
+}
+
+int main() {
+    ::ad::rss::core::RssCheck rssCheck;
+    ::ad::rss::situation::SituationSnapshot situationSnapshot;
+    ::ad::rss::state::RssStateSnapshot rssStateSnapshot;
+    ::ad::rss::state::ProperResponse properResponse;
+    ::ad::rss::world::WorldModel worldModel;
+    ::ad::rss::world::AccelerationRestriction accelerationRestriction;
+    
+    // 设置车辆动力学参数
+    worldModel.timeIndex = 16;
+    worldModel.defaultEgoVehicleRssDynamics = ::ad::rss::world::RssDynamics();
+    worldModel.defaultEgoVehicleRssDynamics.alphaLon.accelMax = ::ad::physics::Acceleration(3.5);
+    worldModel.defaultEgoVehicleRssDynamics.alphaLon.brakeMax = ::ad::physics::Acceleration(-8.);
+    worldModel.defaultEgoVehicleRssDynamics.alphaLon.brakeMin = ::ad::physics::Acceleration(-4.);
+    worldModel.defaultEgoVehicleRssDynamics.alphaLon.brakeMinCorrect = ::ad::physics::Acceleration(-3);
+    worldModel.defaultEgoVehicleRssDynamics.alphaLat.accelMax = ::ad::physics::Acceleration(0.2);
+    worldModel.defaultEgoVehicleRssDynamics.alphaLat.brakeMin = ::ad::physics::Acceleration(-0.8);
+    worldModel.defaultEgoVehicleRssDynamics.responseTime = ::ad::physics::Duration(1.);
+    
+    // 计算车辆位姿数据
+    ::ad::rss::world::Object egoVehicle;
+    ::ad::rss::world::Object otherVehicle;
+    egoVehicle.objectId=23;
+    egoVehicle.objectType=::ad::rss::world::ObjectType::EgoVehicle;
+    calculateOccupiedRegions( egoVehicle.occupiedRegions, 215.3, -364.1 );
+    calculateLatLonVelocities( egoVehicle.velocity );
+
+    otherVehicle.objectId=24;
+    otherVehicle.objectType=::ad::rss::world::ObjectType::OtherVehicle;
+    calculateOccupiedRegions( otherVehicle.occupiedRegions, 225.3, -364.1 );
+    calculateLatLonVelocities( otherVehicle.velocity );
+
+    // 计算道路数据
+    ::ad::rss::world::RoadArea roadArea;
+    ::ad::rss::world::RoadSegment roadSegment;
+    ::ad::rss::world::LaneSegment laneSegment;
+    int length = 50;
+    int width = 5;
+    uint64_t lid;
+    laneSegment.id = -4;
+    if ( laneSegment.id < 0 )
+        laneSegment.drivingDirection = ::ad::rss::world::LaneDrivingDirection::Negative;
+    else
+        laneSegment.drivingDirection = ::ad::rss::world::LaneDrivingDirection::Positive;
+    laneSegment.length.minimum = length;
+    laneSegment.length.maximum = length;
+    laneSegment.width.minimum = width;
+    laneSegment.width.maximum = width;
+    lid = laneSegment.id;
+    if ( lid < 0 ) lid += 0xffff;
+    laneSegment.id = ((uint64_t)43 << 32) + ((uint64_t)1 << 16) + lid;
+    roadSegment.push_back(laneSegment);
+    roadArea.push_back(roadSegment);
+        
+    // 填写场景数组
+    ::ad::rss::world::Scene scene;
+    scene.situationType = ::ad::rss::situation::SituationType::SameDirection;
+    scene.object = otherVehicle;
+    scene.objectRssDynamics = worldModel.defaultEgoVehicleRssDynamics;
+    scene.egoVehicle = egoVehicle;
+    scene.egoVehicleRssDynamics = worldModel.defaultEgoVehicleRssDynamics;
+    scene.egoVehicleRoad = roadArea;
+    worldModel.scenes.push_back(scene);
+    
+    // 调用RSS检查主函数
+    rssCheck.calculateAccelerationRestriction(worldModel, situationSnapshot, rssStateSnapshot, properResponse, accelerationRestriction);
+    
+    return 1;
+}
---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <memory>
+//#include <ad/physics/Acceleration.hpp>
+//#include <ad/rss/world/ObjectId.hpp>
+
+int main() {
+//	::ad::physics::Acceleration a{2.0};
+//	::ad::rss::world::ObjectId id=1234;
+//	printf("hello object %d\n", id);
+//	printf("hello acceleration %f\n", a);
+	uint64_t b = 1<<16;
+	printf("hello %zu\n", ((uint64_t)1)<<16);
+	return 0;
+}
