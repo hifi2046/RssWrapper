96ee5905e14a669058b206b8a92ba95df0d36f84
Huang Yinfei <huangyinfei@huawei.com>
2020-04-28 09:28:46 +0800

    make input valid

--- RssWrapper.cpp
+++ RssWrapper.cpp
@@ -8,10 +8,10 @@
 void calculateOccupiedRegions(::ad::rss::world::OccupiedRegionVector &bounds, float x, float y) {
     ::ad::rss::world::OccupiedRegion r;
     r.segmentId = 43;
-    r.lonRange.minimum=y-0.1;
-    r.lonRange.maximum=y+0.1;
-    r.latRange.minimum=x-0.1;
-    r.latRange.maximum=x+0.1;
+    r.lonRange.minimum=::ad::physics::ParametricValue(y);
+    r.lonRange.maximum=::ad::physics::ParametricValue(y);
+    r.latRange.minimum=::ad::physics::ParametricValue(x);
+    r.latRange.maximum=::ad::physics::ParametricValue(x);
     bounds.push_back(r);
 }
 
@@ -51,7 +51,7 @@ int main() {
 
     otherVehicle.objectId=24;
     otherVehicle.objectType=::ad::rss::world::ObjectType::OtherVehicle;
-    calculateOccupiedRegions( otherVehicle.occupiedRegions, 0.5, 0.7 );
+    calculateOccupiedRegions( otherVehicle.occupiedRegions, 0.5, 0.3 );
     calculateLatLonVelocities( otherVehicle.velocity );
 
     // 计算道路数据
@@ -61,7 +61,7 @@ int main() {
     int length = 50;
     int width = 5;
     uint64_t lid;
-    laneSegment.id = -4;
+    laneSegment.id = 43;
     if ( laneSegment.id < 0 )
         laneSegment.drivingDirection = ::ad::rss::world::LaneDrivingDirection::Negative;
     else
@@ -70,9 +70,9 @@ int main() {
     laneSegment.length.maximum = length;
     laneSegment.width.minimum = width;
     laneSegment.width.maximum = width;
-    lid = laneSegment.id;
-    if ( lid < 0 ) lid += 0xffff;
-    laneSegment.id = ((uint64_t)43 << 32) + ((uint64_t)1 << 16) + lid;
+//    lid = laneSegment.id;
+//    if ( lid < 0 ) lid += 0xffff;
+//    laneSegment.id = ((uint64_t)43 << 32) + ((uint64_t)1 << 16) + lid;
     roadSegment.push_back(laneSegment);
     roadArea.push_back(roadSegment);
         
@@ -88,6 +88,51 @@ int main() {
     
     // 调用RSS检查主函数
     rssCheck.calculateAccelerationRestriction(worldModel, situationSnapshot, rssStateSnapshot, properResponse, accelerationRestriction);
+
+    // 查看状况中间结果
+    std::cout << "\033[31msituationSnapshot\033[0m: timeIndex: " << situationSnapshot.timeIndex;
+    std::cout << ", size: " << situationSnapshot.situations.size() << std::endl;
+    int count = 0;
+    for( auto &t : situationSnapshot.situations ) {
+        std::cout << "  -" << std::endl;
+        std::cout << "    objectId: " << t.objectId << std::endl;
+        std::cout << "    situationId: " << t.situationId << std::endl;
+        std::cout << "    situationType: " << t.situationType << std::endl;
+//        std::cout << "    egoVehicleState: " << t.egoVehicleState << std::endl;
+//        std::cout << "    otherVehicleState: " << t.otherVehicleState << std::endl;
+        std::cout << "    relativePosition: " << t.relativePosition << std::endl;
+        count++;
+    }
     
+    // 查看状态中间结果
+    std::cout << "\033[31mrssStateSnapshot\033[0m: timeIndex: " << rssStateSnapshot.timeIndex;
+    std::cout << ", size: " << rssStateSnapshot.individualResponses.size() << std::endl;
+    count = 0;
+    for( auto &t : rssStateSnapshot.individualResponses ) {
+        std::cout << "  -" << std::endl;
+        std::cout << "    objectId: " << t.objectId << std::endl;
+        std::cout << "    situationId: " << t.situationId << std::endl;
+        std::cout << "    longitudinalState: " << t.longitudinalState << std::endl;
+        std::cout << "    lateralStateRight: " << t.lateralStateRight << std::endl;
+        std::cout << "    lateralStateLeft: " << t.lateralStateLeft << std::endl;
+        count++;
+    }
+    
+    // 查看正确响应中间结果
+    std::cout << "\033[31mproperResponse\033[0m: timeIndex: " << properResponse.timeIndex << std::endl;
+    auto &t = properResponse;
+    std::cout << "  isSafe: " << t.isSafe << std::endl;
+    std::cout << "  dangerousObjects: " << t.dangerousObjects << std::endl;
+    std::cout << "  longitudinalResponse: " << t.longitudinalResponse << std::endl;
+    std::cout << "  lateralResponseRight: " << t.lateralResponseRight << std::endl;
+    std::cout << "  lateralResponseLeft: " << t.lateralResponseLeft << std::endl;
+
+    // 查看安全限制
+    std::cout << "\033[31maccelerationRestriction\033[0m: timeIndex: " << accelerationRestriction.timeIndex << std::endl;
+    auto &p = accelerationRestriction;
+    std::cout << "  longitudinalRange: " << p.longitudinalRange << std::endl;
+    std::cout << "  lateralRightRange: " << p.lateralRightRange << std::endl;
+    std::cout << "  lateralLeftRange: " << p.lateralLeftRange << std::endl;
+
     return 1;
 }
